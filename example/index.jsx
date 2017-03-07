210aef783af56426b088d3857105172a01d6aab4
锡炎 <xiyan.xxm@alibaba-inc.com>
2017-03-07 11:38:43 +0800

    feature. add base code.

---
+++ example/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  Progress,
+} from '../lib';
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <Progress />
+    );
+  }
+}
